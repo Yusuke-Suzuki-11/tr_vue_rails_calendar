294c3cdb4bda5846147b67a4f30842c6d3a94b3e
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-11-18 09:50:16 +0900

    component作成

--- frontend/src/components/CalendarDetails.vue
+++ frontend/src/components/CalendarDetails.vue
@@ -1,25 +1,22 @@
 <template>
   <div>
-    <h1>Calendar</h1>
-    <ul>
-      <li v-for="event in events" :key="event.id">
-        {{ event.name }}
-      </li>
-    </ul>
+    <h1>CalendarDetails</h1>
+    <div v-for="event in events" :key="event.id">
+      <h2>{{ event.name }}</h2>
+      <p>start: {{ event.start }}</p>
+      <p>end: {{ event.end }}</p>
+      <p>description: {{ event.description }}</p>
+    </div>
+
     <button type="submit" @click="fetchEvents()">fetchEvents</button>
-    <CalendarDetails />
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import CalendarDetails from "./CalendarDetails.vue";
 
 export default {
-  name: "CustomCalendar",
-  components: {
-    CalendarDetails,
-  },
+  name: "CalendarDetails",
   data: () => ({
     events: [],
   }),
@@ -31,7 +28,7 @@ export default {
           this.events = response.data;
         })
         .catch((error) => {
-          console.log(error);
+          console.error(error);
         });
     },
   },
