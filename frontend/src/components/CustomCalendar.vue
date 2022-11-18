0d90604d504df120c223c1746fd640cd1f38f3a0
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-11-18 09:32:20 +0900

    微調整

--- frontend/src/components/CustomCalendar.vue
+++ frontend/src/components/CustomCalendar.vue
@@ -3,15 +3,28 @@
     <h1>Calendar</h1>
     <p>events:</p>
     <p>{{ events }}</p>
-    <button type="submit">fetch Events</button>
+    <button type="submit" @click="fetchEvents()">fetchEvents</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "CustomCalendar",
   data: () => ({
     events: [],
   }),
+  methods: {
+    fetchEvents() {
+      axios
+        .get("http://localhost:3000/events")
+        .then((response) => {
+          this.events = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
